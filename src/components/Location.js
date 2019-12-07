36733bc9cd518edbe35233733495784ab19c6904
Matias Nahuel Améndola <soporte.esolutions@gmail.com>
2019-12-07 18:46:11 -0300

    Destructuring
    -

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 // functional component
 const Location = (props) => {
-    console.log(props);
-    debugger;
-    return (<div><h1>{props.city}</h1></div>)
+    // destructuring
+    const { city } = props;
+
+    return (
+        <div><h1>{city}</h1></div>
+    )
 };
 
 export default Location;
